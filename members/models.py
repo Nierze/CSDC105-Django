c15faea311907144d7140be1b2351c5029e22ab7
MegRivera <116321876+Nierze@users.noreply.github.com>
2024-04-24 00:14:49 +0800

    changed email
    sf

--- members/models.py
+++ members/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Member(models.Model): 
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
-
+    # 
     def __repr__(self):
         return self.firstname +  " " + self.lastname
 
